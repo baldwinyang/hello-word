4dadbb32bc6dc5dc03e37f38ae4a15daabf470d4
baldwinyang <baldwinyang@126.com>
2019-10-30 19:10:57 +0800

    IDEA上传测试

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test{
+    public static void main(String[] args) {
+        int t=10000;
+        Integer i=new Integer(t);
+        String s=i.toString();
+        System.out.println(s);
+        char c='杨';//不同的编码方式长度也不一样
+        System.out.println(c);
+       // System.arraycopy();
+        Map map =new HashMap();
+    }
+}
